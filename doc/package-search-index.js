acac9aaaea3bdef99ba1f3326f7283f7be7c3dae
JavierRuiz <javieruizma@gmail.com>
2024-05-29 09:30:52 +0200

    Proyecto documentado

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"ExamenED2b_JavierRuiz","l":"examen"}];updateSearchResults();
--- src/examen/Hora.java
+++ src/examen/Hora.java
@@ -1,5 +1,31 @@
 package examen;
 
 public class Hora {
-
+	/**
+	 * @author Javier Ruiz
+	 * @version 1.0
+	 */
+	public static boolean horaValida;
+/**
+ * Es un método para comprobar si la hora es válida o correcta, ya que va comprobando los valores de la hora minutos y segundos.
+ * @param segundos
+ * @param minutos
+ * @param horas
+ * @return Devuelve el valor de horaValida.
+ */
+	public static boolean validarHora(int segundos, int minutos, int horas) {
+		horaValida = true;
+		if ((horas >= 0) && (minutos >= 0) && (segundos >= 0)) {
+			if (horas > 23) {
+				horaValida = false;
+			} else if (minutos > 59) {
+				horaValida = false;
+			} else if (segundos > 59) {
+				horaValida = false;
+			}
+		} else {
+			horaValida = false;
+		}
+		return horaValida;
+	}
 }
